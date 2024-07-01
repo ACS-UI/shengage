0890eb066e5c556e8133b7b4c57d4095392eac16
sarthak bansal <sarthak.x.bansal@goindigo.in>
2024-07-01 16:13:49 +0530

    Stories

--- blocks/stories/stories.js
+++ blocks/stories/stories.js
@@ -20,7 +20,7 @@ export default function decorate(block) {
 
   const dotContainer = document.createElement('div');
   dotContainer.classList.add('dot-container');
-  const dots = ['dot1 active', 'dot2', 'arrow'];
+  const dots = ['dot dot1 active', 'dot dot2', 'dot dot3'];
   dots.forEach((className) => {
     const div = document.createElement('div');
     className.split(' ').forEach((cls) => {
@@ -30,38 +30,24 @@ export default function decorate(block) {
   });
   block.append(dotContainer);
 
-  // start of carousal code
-  const buttonBack = document.querySelector('.dot1');
-  const buttonNext = document.querySelector('.dot2');
-  const listOfstoryElements = document.querySelectorAll('.story-slide');
-  const storyContainer = document.querySelector('.story-container');
-  let currentstory = 0;
+  const dotts = document.querySelectorAll('.dot');
+  const carousel = document.querySelector('.story-container');
 
-  function setScrollTo() {
-    const scrollLeft = currentstory * listOfstoryElements[0].offsetWidth;
-    storyContainer.scrollTo({ left: scrollLeft, behavior: 'smooth' });
-  }
-
-  buttonBack.addEventListener('click', () => {
-    if (currentstory > 0) {
-      currentstory -= 1;
-    }
-    setScrollTo();
-  });
-
-  buttonNext.addEventListener('click', () => {
-    if (currentstory < listOfstoryElements.length - 1) {
-      currentstory += 1;
-    }
-    setScrollTo();
-  });
-
-  listOfstoryElements.forEach((storyElement, index) => {
-    storyElement.addEventListener('click', () => {
-      currentstory = index;
-      const scrollLeft = currentstory * listOfstoryElements[0].offsetWidth;
-      storyContainer.scrollTo({ left: scrollLeft, behavior: 'smooth' });
+  dotts.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+      let offset = 0.5;
+      dotts.forEach((d) => d.classList.remove('active'));
+      dot.classList.add('active');
+      if (index === 1) {
+        offset = -67;
+      } else if (index === 2) {
+        offset = -101;
+      }
+      carousel.style.transform = `translateX(${offset}%)`;
     });
   });
+
+  dotts[0].classList.add('active');
+  carousel.style.transform = 'translateX(0.5%)';
   // carousal code end
 }
