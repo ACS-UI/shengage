f9509c965a2659c23676258c7df97e32744d8477
sarthak bansal <sarthak.x.bansal@goindigo.in>
2024-07-13 10:24:27 +0530

    fix

--- blocks/shengage-events/shengage-events.js
+++ blocks/shengage-events/shengage-events.js
@@ -5,23 +5,20 @@ export default function decorate(block) {
   block.classList.add(`columns-${cols.length}-cols`);
   [...block.children].forEach((row) => {
     [...row.children].forEach((col, id) => {
-        col.classList.add(`col-${id}`);
+      col.classList.add(`col-${id}`);
     });
-    });
+  });
 
-    const textFields = block.querySelector('.col-0');
-    let i = 1;
-    for (const child of textFields.children) {    
-      console.log(child.tagName);
-      if(child.tagName === 'TABLE'){
-        child.classList.add('card');
-        child.classList.add(`impactcard${i}`);
-      }
-      else{      
+  const textFields = block.querySelector('.col-0');
+  let i = 1;
+  for (const child of textFields.children) {
+    console.log(child.tagName);
+    if (child.tagName === 'TABLE') {
+      child.classList.add('card');
+      child.classList.add(`impactcard${i}`);
+    } else {
       child.classList.add(`textfield${i}`);
     }
-      i++;
-    }
-
-
+    i++;
+  }
 }
