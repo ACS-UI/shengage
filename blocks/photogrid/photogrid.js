6b3dc1e4713a4dcd7657f41c3339d3fea8edc3ca
sarthak bansal <sarthak.x.bansal@goindigo.in>
2024-07-26 12:44:51 +0530

    fix photogrid

--- blocks/photogrid/photogrid.js
+++ blocks/photogrid/photogrid.js
@@ -1,11 +1,11 @@
 export default function decorate(block) {
-    const photoList = block.querySelectorAll('ul li');
-    
-    photoList.forEach((photoEl) => {
-        let imagePath = photoEl.querySelector('img').src;
-        let popUpBox = document.createElement('div');
-        popUpBox.classList.add('popup-container');
-        popUpBox.innerHTML = `<div class='button-container'>
+  const photoList = block.querySelectorAll('ul li');
+
+  photoList.forEach((photoEl) => {
+    const imagePath = photoEl.querySelector('img').src;
+    const popUpBox = document.createElement('div');
+    popUpBox.classList.add('popup-container');
+    popUpBox.innerHTML = `<div class='button-container'>
         <button class='fullscreen-btn'>
          <img src='./assets/full-screen.svg'>
         </button>
@@ -25,79 +25,78 @@ export default function decorate(block) {
         <img src='./assets/next-circle.svg'>
         </button>
         </div>`;
-        block.append(popUpBox);
+    block.append(popUpBox);
 
-        photoEl.addEventListener('click', () => {
-            document.querySelector('.popup-image .fullimage').setAttribute('src', imagePath);
-            openModal();
-        });
-
-        const closeBtn = popUpBox.querySelector('.close-btn');
-        const fullScreen = popUpBox.querySelector('.fullscreen-btn');
-        const closeFullScreen = popUpBox.querySelector('.closefullscreen-btn');
-        closeBtn?.addEventListener('click', () => {
-            closeModal();
-        });
-        
-        fullScreen?.addEventListener('click', () => {
-            openFullscreen();
-            closeFullScreen.style.display = 'block';
-            fullScreen.style.display = 'none';
-        });
-
-        closeFullScreen?.addEventListener('click', () => {
-            closeFullscreen();
-            closeFullScreen.style.display = 'none';
-            fullScreen.style.display = 'block';
-        });
+    photoEl.addEventListener('click', () => {
+      document.querySelector('.popup-image .fullimage').setAttribute('src', imagePath);
+      openModal();
     });
 
-    function openModal() {
-        document.querySelector('.popup-container').style.display = 'block';
-    }
-
-    function closeModal() {
-        document.querySelector('.popup-container').style.display = "none";
-    }
-    const elem = document.documentElement;
-    function openFullscreen() {
-        if (elem.requestFullscreen) {
-          elem.requestFullscreen();
-        } else if (elem.webkitRequestFullscreen) { 
-          elem.webkitRequestFullscreen();
-        } else if (elem.msRequestFullscreen) {
-          elem.msRequestFullscreen();
-        }
-    }
-
-    function closeFullscreen() {
-        if (document.exitFullscreen) {
-          document.exitFullscreen();
-        } else if (document.webkitExitFullscreen) { 
-          document.webkitExitFullscreen();
-        } else if (document.msExitFullscreen) { 
-          document.msExitFullscreen();
-        }
-      }
-
-    const nextBtn = document.querySelector('.next');
-    const prevBtn = document.querySelector('.prev');  
-
-    let imgIndex = 0;
-    prevBtn?.addEventListener('click', () => {
-        imgIndex = imgIndex -= 1;
-        if (imgIndex < 0) imgIndex = photoList.length - 1;
-        showSlides(imgIndex);
+    const closeBtn = popUpBox.querySelector('.close-btn');
+    const fullScreen = popUpBox.querySelector('.fullscreen-btn');
+    const closeFullScreen = popUpBox.querySelector('.closefullscreen-btn');
+    closeBtn?.addEventListener('click', () => {
+      closeModal();
     });
 
-    nextBtn?.addEventListener('click', () => {     
-        imgIndex = (imgIndex += 1) % photoList.length;
-        showSlides(imgIndex);
+    fullScreen?.addEventListener('click', () => {
+      openFullscreen();
+      closeFullScreen.style.display = 'block';
+      fullScreen.style.display = 'none';
     });
 
-    function showSlides(n) {
-        let nextImageSrc = photoList[n].querySelector('img').src;
-        document.querySelector('.popup-image .fullimage').setAttribute('src', nextImageSrc);
-    }
+    closeFullScreen?.addEventListener('click', () => {
+      closeFullscreen();
+      closeFullScreen.style.display = 'none';
+      fullScreen.style.display = 'block';
+    });
+  });
 
-} 
+  function openModal() {
+    document.querySelector('.popup-container').style.display = 'block';
+  }
+
+  function closeModal() {
+    document.querySelector('.popup-container').style.display = 'none';
+  }
+  const elem = document.documentElement;
+  function openFullscreen() {
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) {
+      elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) {
+      elem.msRequestFullscreen();
+    }
+  }
+
+  function closeFullscreen() {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    }
+  }
+
+  const nextBtn = document.querySelector('.next');
+  const prevBtn = document.querySelector('.prev');
+
+  let imgIndex = 0;
+  prevBtn?.addEventListener('click', () => {
+    imgIndex = imgIndex -= 1;
+    if (imgIndex < 0) imgIndex = photoList.length - 1;
+    showSlides(imgIndex);
+  });
+
+  nextBtn?.addEventListener('click', () => {
+    imgIndex = (imgIndex += 1) % photoList.length;
+    showSlides(imgIndex);
+  });
+
+  function showSlides(n) {
+    const nextImageSrc = photoList[n].querySelector('img').src;
+    document.querySelector('.popup-image .fullimage').setAttribute('src', nextImageSrc);
+  }
+}
