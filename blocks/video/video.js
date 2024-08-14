e660e53fdf23715b2dbe1258f1b788e337b63269
sarthak bansal <sarthak.x.bansal@goindigo.in>
2024-08-14 11:38:44 +0530

    unmte

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -3,13 +3,26 @@ export default function decorate(block) {
   block.innerHTML = '';
   block.innerHTML = `
     <div class="video-background">
-        <video autoplay loop  playsinline class="desktop">
+        <video autoplay loop muted playsinline id="video" class="desktop">
             <source src="${videourl.innerText}" type="video/mp4">            
         </video>
-        </div>
-        <div class="video-overlay">
+    </div>
+    <button id="unmute" class="unmute-button">Unmute</button>
+    <div class="video-overlay">
         <div class="hero-text">
           ${videoText.innerText}
         </div>        
     </div>`;
+
+  const video = block.querySelector('#video');
+  const unmute = block.querySelector('#unmute');
+
+  // Event listener to unmute the video
+  unmute.addEventListener('click', () => {
+    if (video.muted) {
+      video.muted = false;
+      video.play();
+      unmute.style.display = 'none'; // Hide the unmute button after it's clicked
+    }
+  });
 }
