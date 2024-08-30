a1b3cc45628f11206e8192266ef0b793e4db59eb
karvannanpa <karpandu@adobe.com>
2024-08-30 22:24:53 +0530

    added action count

--- blocks/comment/comment.js
+++ blocks/comment/comment.js
@@ -257,7 +257,7 @@ async function handleEventDelegation(event, comments) {
     const isLiked = target.classList.toggle('liked');
     const currentComment = await getCommentById(comments, commentId);
     let likeCount = currentComment.likedBy.length;
-    if (likeCount) {
+    if (likeCount > 1) {
       likeCount = isLiked ? likeCount : likeCount - 1;
     } else {
       likeCount = 1;
