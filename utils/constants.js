a00c29087be2b84e9280877c7e6afd7b797bd1d7
sarthak bansal <sarthak.x.bansal@goindigo.in>
2024-09-04 14:21:12 +0530

    lint

--- utils/constants.js
+++ utils/constants.js
@@ -1,7 +1,7 @@
-let DEV_LAUNCH_SCRIPT = 'https://assets.adobedtm.com/4861475e0aeb/0d0ceb0ee381/launch-66ff90804336-development.min.js' ;
-let PROD_LAUNCH_SCRIPT ='';
+const DEV_LAUNCH_SCRIPT = 'https://assets.adobedtm.com/4861475e0aeb/0d0ceb0ee381/launch-66ff90804336-development.min.js';
+const PROD_LAUNCH_SCRIPT = '';
 
 export default {
- DEV_LAUNCH_SCRIPT,
- PROD_LAUNCH_SCRIPT
-}
+  DEV_LAUNCH_SCRIPT,
+  PROD_LAUNCH_SCRIPT,
+};
