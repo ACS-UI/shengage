0cfefefcc9ac0126dabe64e740ff31dc1d2620e0
sarthak bansal <sarthak.x.bansal@goindigo.in>
2024-09-05 16:19:29 +0530

    removed async

--- utils/common.js
+++ utils/common.js
@@ -9,7 +9,7 @@ const addLaunchScript = () => {
   }
   const script = document.createElement('script');
   script.src = source;
-  script.async = true;
+  script.async = false;
   document.head.appendChild(script);
 };
 
