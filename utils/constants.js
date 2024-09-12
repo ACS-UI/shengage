e49e81c4b8decc3f3e25633463d00020096722c4
sarthak bansal <sarthak.x.bansal@goindigo.in>
2024-09-12 21:58:48 +0530

    test

--- utils/constants.js
+++ utils/constants.js
@@ -1,7 +1,8 @@
 const DEV_LAUNCH_SCRIPT = 'https://assets.adobedtm.com/4861475e0aeb/0d0ceb0ee381/launch-66ff90804336-development.min.js';
 const PROD_LAUNCH_SCRIPT = 'https://assets.adobedtm.com/4861475e0aeb/0d0ceb0ee381/launch-12868fd25acc.min.js';
-
+const LOCAL = 'localhost';
 export default {
   DEV_LAUNCH_SCRIPT,
   PROD_LAUNCH_SCRIPT,
+  LOCAL,
 };
