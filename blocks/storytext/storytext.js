660533a6463239bb3aeb73c1b8b5a336516b461e
swatiadobe <swatijain@adobe.com>
2024-09-13 11:15:24 +0530

    lint issue resolved

--- blocks/storytext/storytext.js
+++ blocks/storytext/storytext.js
@@ -1,6 +1,6 @@
 export default function decorate(block) {
-    const divs = [...block.children];
-    divs.forEach((div, id) => {
-      div.classList.add(`box-${id}`);
-    });
+  const divs = [...block.children];
+  divs.forEach((div, id) => {
+    div.classList.add(`box-${id}`);
+  });
 }
