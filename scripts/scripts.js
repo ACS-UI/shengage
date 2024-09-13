d3d8abc5edddc7a83351f9fcfe12464c4a761669
karvannanpa <karpandu@adobe.com>
2024-09-13 12:57:00 +0530

    updated button colors

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -80,7 +80,7 @@ export function getConfig() {
   }
   const ims = {
     client_id: 'shengage',
-    environment: 'prod',
+    environment: 'stage',
   };
 
   window.shengage = window.shengage || {};
