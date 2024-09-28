d1fa49dcb1ce5a1ca7a9f12a07ecd5eb2ba126a6
karvannanpa <karpandu@adobe.com>
2024-09-28 06:17:10 +0530

    updated scope

--- scripts/config.js
+++ scripts/config.js
@@ -6,27 +6,11 @@ export default function getConfig() {
     return window.shengage.config;
   }
 
-  const HOSTS = [
-    {
-      env: 'PROD',
-      hlxPreview: 'main--shengage--acs-ui.hlx.page',
-      hlxLive: 'main--shengage--acs-ui.hlx.live',
-    },
-    {
-      env: 'STAGE',
-      hlxPreview: 'stage--shengage--acs-ui.hlx.page',
-      hlxLive: 'stage--shengage--acs-ui.hlx.live',
-    },
-    {
-      env: 'DEV',
-      hlxPreview: 'dev--shengage--acs-ui.hlx.page',
-      hlxLive: 'dev--shengage--acs-ui.hlx.live',
-    },
-  ];
+  const HOSTS = ['main--shengage--acs-ui.hlx.page', 'main--shengage--acs-ui.hlx.live', 'https://shengage.adobe.com'];
 
   const currentHost = window.location.hostname;
-  const currentEnv = HOSTS.find((hostObj) => Object.values(hostObj).includes(currentHost));
-  const isProd = currentEnv?.env === 'PROD' || currentEnv?.authorUrl === 'author-p122525-e1219150.adobeaemcloud.com';
+  const currentEnv = HOSTS.includes(currentHost);
+  const isProd = currentEnv === 'PROD';
   const ppsOrigin = isProd ? 'https://pps.adobe.io' : 'https://pps-stage.adobe.io';
   const ims = {
     client_id: 'shengage',
@@ -36,7 +20,7 @@ export default function getConfig() {
   window.shengage = window.shengage || {};
   window.shengage.config = {
     ims,
-    adobeIoEndpoint: `https://51837-shengageapp${!isProd ? '-dev' : ''}.adobeioruntime.net/api/v1/web/shengage/`,
+    adobeIoEndpoint: `https://51837-shengageapp${!isProd ? '-stage' : ''}.adobeioruntime.net/api/v1/web/shengage`,
     giphyApiKey: 'vcXGLBipjtwyEqhVQgBf8yfU6wAegwA3',
     ppsOrigin,
   };
