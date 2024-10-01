f9b1cc4d1ffcf59139868d3ab94ddbee07ad4734
sarthak bansal <sarthak.x.bansal@goindigo.in>
2024-10-01 16:03:41 +0530

    added page load

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,7 +1,9 @@
 // eslint-disable-next-line import/no-cycle
+import { pushAnalytics } from '../utils/Analytics.js';
 import { sampleRUM } from './aem.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
+pushAnalytics({ event: 'pageLoaded' });
 
 // add more delayed functionality here
