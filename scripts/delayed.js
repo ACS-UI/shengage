d2cb754123daaaa867856ac22911769a16401a75
sarthak bansal <sarthak.x.bansal@goindigo.in>
2024-10-01 16:07:25 +0530

    fix

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/no-cycle
-import { pushAnalytics } from '../utils/Analytics.js';
+import { pushAnalytics } from '../utils/analytics.js';
 import { sampleRUM } from './aem.js';
 
 // Core Web Vitals RUM collection
