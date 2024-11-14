c2146b06603611ae7c63af8071e7192b12941f7b
swatiadobe <swatijain@adobe.com>
2024-11-14 21:56:03 +0530

    story slide order updated

--- blocks/stories/stories.js
+++ blocks/stories/stories.js
@@ -29,7 +29,7 @@ export default function decorate(block) {
       }
     });
 
-    ul.append(li);
+    ul.prepend(li);
   });
   ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
